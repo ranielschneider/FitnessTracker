b1fcedfa0a90f6cecf4ccdb02aba7c63d31f6e03
Raniel Schneider <ranielschneider@hotmail.com>
2025-04-22 13:03:13 +0100

    Atualização de UI e inicio de validação do botão da tela imc.

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/ImcActivity.kt
@@ -1,12 +1,20 @@
 package co.tiagoaguiar.fitnesstracker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ImcActivity : AppCompatActivity() {
+
+    private lateinit var editWeight: EditText
+    private lateinit var editHeight: EditText
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,24 @@ class ImcActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+
+        btnSend.setOnClickListener {
+            if (!validate()) {
+                Toast.makeText(this, R.string.field_messages, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
     }
-}
+
+    private fun validate(): Boolean {
+        return (editWeight.text.toString().isNotEmpty()
+                && editHeight.text.toString().isNotEmpty()
+                && !editWeight.text.toString().startsWith("0")
+                && !editHeight.text.toString().startsWith("0"))
+        }
+
+    }
