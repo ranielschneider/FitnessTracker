1a50e22cb8ef960e477533cd10f11351438b414f
Raniel Schneider <ranielschneider@hotmail.com>
2025-04-29 12:14:10 +0100

    List

--- app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/fitnesstracker/MainActivity.kt
@@ -1,58 +1,75 @@
 package co.tiagoaguiar.fitnesstracker
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.LinearLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var btnImc: LinearLayout
+
     private lateinit var rvMain: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val adapter = MainAdapter()
+        val mainItems = mutableListOf<MainItem>()
+        mainItems.add(
+            MainItem(
+                id = 1,
+                drawableId = R.drawable.baseline_wb_sunny_24,
+                textStringId = R.string.label_imc,
+                color = Color.GREEN
+            )
+        )
+        mainItems.add(
+            MainItem(
+                id = 2,
+                drawableId = R.drawable.baseline_wb_sunny_24,
+                textStringId = R.string.label_tmb,
+                color = Color.GREEN
+            )
+        )
+
+
+        val adapter = MainAdapter(mainItems)
         rvMain = findViewById(R.id.rv_main)
         rvMain.adapter = adapter
         rvMain.layoutManager = LinearLayoutManager(this)
 
-
-
-
-
-//        btnImc = findViewById(R.id.btn_imc)
-//
-//        btnImc.setOnClickListener {
-//            val i = Intent(this, ImcActivity::class.java)
-//            startActivity(i)
-//        }
-
-
     }
-    private inner class MainAdapter: RecyclerView.Adapter<MainViewHolder>(){
+
+    private inner class MainAdapter(private val mainItems: List<MainItem>) :
+        RecyclerView.Adapter<MainViewHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
             val view = layoutInflater.inflate(R.layout.main_item, parent, false)
-            return  MainViewHolder(view)
+            return MainViewHolder(view)
 
         }
 
         override fun getItemCount(): Int {
-            return 30
+            return mainItems.size
         }
 
         override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+            val itemCurrent = mainItems[position]
+            holder.bind(itemCurrent)
         }
 
     }
 
-    private class MainViewHolder (view: View): RecyclerView.ViewHolder(view) {
+    private class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: MainItem) {
+            val buttonTest: Button = itemView.findViewById(R.id.btn_item)
+            buttonTest.setText(item.textStringId)
+        }
 
     }
 }
